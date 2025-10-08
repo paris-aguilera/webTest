d555397f95ffc1294daff4d2e4752b31f4ee51fe
paris <parisaguilera@gmail.com>
2025-10-08 04:54:25 -0300

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://paris-aguilera.github.io',
+  base: 'webTest',
   vite: {
     plugins: [tailwindcss()]
   }

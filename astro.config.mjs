aaea2557f3594a1a3e2cc51567e36228c75dc77c
paris <parisaguilera@gmail.com>
2025-10-20 02:41:12 -0300

    nuevos cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://paris-aguilera.github.io',
-  base: 'webTest',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
